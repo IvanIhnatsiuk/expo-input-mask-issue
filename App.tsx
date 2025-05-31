f34f7c59a91f36872a912e2e0b7fa89dee28b5ee
dominic-preap <preapchanoudom@gmail.com>
2025-05-31 16:51:02 +0700

    fix: add demo input mask

--- App.tsx
+++ App.tsx
@@ -1,11 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { MaskedTextInput } from "react-native-advanced-input-mask";
 
 export default function App() {
+  const [value, setValue] = useState(10);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Testing</Text>
+      <MaskedTextInput
+        allowedKeys="0123456789,."
+        autocomplete={false}
+        keyboardType="numeric"
+        mask="$[09999]{.}[09]"
+        multiline
+        onChangeText={(_, extractedValue) => {
+          setValue(+extractedValue);
+        }}
+        placeholder="$5"
+        value={`${value}`}
+        style={{
+          borderWidth: 1,
+          width: 120,
+          borderColor: "black",
+          fontWeight: "bold",
+          padding: 12,
+          textAlign: "center",
+        }}
+      />
+
+      <View style={{ flexDirection: "row", gap: 12 }}>
+        {[10, 20, 30, 40, 50].map((x) => (
+          <Pressable
+            key={x}
+            onPress={()=>setValue(x)}
+            style={{
+              borderRadius: 99,
+              alignItems: "center",
+              width: 40,
+              padding: 8,
+              backgroundColor: x === value? "#73D4FF": '#DFF1FE',
+            }}
+          >
+            <Text>{x}</Text>
+          </Pressable>
+        ))}
+      </View>
+
+      <Text>Value should update as above: {value}</Text>
     </View>
   );
 }
@@ -13,8 +55,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    gap: 24,
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,25 @@
+# Expo Demo
+
+## Installation
+
+```bash
+yarn install
+```
+
+## Running the app
+
+```bash
+yarn ios
+```
+
+## Issues
+
+> Demo on bugs [issues/#123](https://github.com/IvanIhnatsiuk/react-native-advanced-input-mask/issues/113) of `react-native-advanced-input-mask`
+
+MaskInput change event on working using expo development build
+
+- Try change value in the input, value state is not changing
+- Also, update state value does not update value in input also (try click on the badge number)
+
+![screenshot](./screenshot.png)
+
